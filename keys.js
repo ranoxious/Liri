7e931bcc792b7ae467e6eb5600a9cf1b88befda3
Ranoxious <jumpship3456@gmail.com>
2018-07-24 01:40:58 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,15 @@
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 'EyLDDETv1tIMxA3xdRsxUFXfu',
+  consumer_secret: 'nntfhX1RaVMvpKMXzlF01JOMlIFuAZedPfBdJVZyfafUvMgbgZ'
+  ,
+  access_token_key: '1019391663238778880-IL70nTEgiYlOS69XFUItggXAcojuHC',
+  access_token_secret: 'Ot1YOKjA4rVMr6AqWHzk4lqtDGUlMcVa34Tt6I92o0Lar'
+};
+
+exports.spotify = {
+  id: 'ac0461f9a165468aadaf8a29027f2bfe',
+  secret: '3c018f995b3c41ce9910c059593da54e'
+};
---
+++ liri.js
@@ -0,0 +1,221 @@
+var fs = require("fs");
+var keys = require("./keys.js");
+var figlet = require("figlet");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+
+switch (action) {
+    case "my-tweets":
+        twitter();
+        break;
+
+    case "spotify-this-song":
+        spotify();
+        break;
+
+    case "movie-this":
+        omdb();
+        break;
+
+    case "do-what-it-says":
+        doIt();
+        break;
+}
+
+
+function twitter() {
+
+    figlet('Twitter API', function (err, data) {
+        console.log('\n');
+        console.log(data)
+    });
+
+    var client = new Twitter(keys);
+    var params = {
+        screen_name: 'AsherSlime'
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+            for (var i = 0; i < tweets.length; i++) {
+
+                console.log('Tweet: ' + tweets[i].text + ' / Created: ' + tweets[i].created_at);
+            }
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+        }
+    });
+}
+
+function spotify() {
+
+    figlet('Spotify API', function (err, data) {
+        console.log('\n');
+        console.log(data)
+    });
+
+    var spotify = new Spotify({
+        id: 'ac0461f9a165468aadaf8a29027f2bfe',
+        secret: '3c018f995b3c41ce9910c059593da54e'
+    });
+
+    if (value === undefined) {
+
+        spotify.search({
+            type: 'track',
+            limit: 1,
+            query: 'El Perdon'
+        }, function (err, data) {
+
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            var jsonResults = JSON.stringify(data.tracks.items[0]);
+            var artistsName = JSON.stringify('Artist Name: ' + data.tracks.items[0].album.artists[0].name);
+            var songName = JSON.stringify('Song Name: ' + data.tracks.items[0].name);
+            var previewLink = JSON.stringify('Preview Link: ' + data.tracks.items[0].preview_url);
+            var albumName = JSON.stringify('Album Name: ' + data.tracks.items[0].album.name);
+
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+            console.log(artistsName);
+            console.log(songName);
+            console.log(previewLink);
+            console.log(albumName);
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+
+        });
+
+    } else {
+
+        spotify.search({
+            type: 'track',
+            limit: 1,
+            query: value
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var jsonResults = JSON.stringify(data.tracks.items[0]);
+            var artistsName = JSON.stringify('Artist Name: ' + data.tracks.items[0].album.artists[0].name);
+            var songName = JSON.stringify('Song Name: ' + data.tracks.items[0].name);
+            var previewLink = JSON.stringify('Preview Link: ' + data.tracks.items[0].preview_url);
+            var albumName = JSON.stringify('Album Name: ' + data.tracks.items[0].album.name);
+
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+            console.log(artistsName);
+            console.log(songName);
+            console.log(previewLink);
+            console.log(albumName);
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+
+        });
+    }
+}
+
+function omdb() {
+
+    figlet('ODMb API', function (err, data) {
+        console.log('\n');
+        console.log(data)
+    });
+
+    if (value === undefined) {
+
+        defaultSearch = 'http://www.omdbapi.com/?apikey=e05ec029&t=hotel+transylvania'
+        request(defaultSearch, function (error, response) {
+            var results = JSON.parse(response.body);
+
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+            console.log('Movie Title: ' + results.Title);
+            console.log('Release Year: ' + results.Year);
+            console.log(results.Ratings[0].Source + ' ' + results.Ratings[0].Value);
+            console.log(results.Ratings[1].Source + ' ' + results.Ratings[1].Value);
+            console.log('Country: ' + results.Country);
+            console.log('Language: ' + results.Language);
+            console.log('Plot: ' + results.Plot);
+            console.log('Actors: ' + results.Actors);
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+
+        });
+
+    } else {
+
+        var movieQuery = 'http://www.omdbapi.com/?apikey=e05ec029&t=' + value;
+        request(movieQuery, function (error, response) {
+            var results = JSON.parse(response.body);
+
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+            console.log('Movie Title: ' + results.Title);
+            console.log('Release Year: ' + results.Year);
+            console.log(results.Ratings[0].Source + ' ' + results.Ratings[0].Value);
+            console.log(results.Ratings[1].Source + ' ' + results.Ratings[1].Value);
+            console.log('Country: ' + results.Country);
+            console.log('Language: ' + results.Language);
+            console.log('Plot: ' + results.Plot);
+            console.log('Actors: ' + results.Actors);
+            console.log('\n');
+            console.log('            ===================================');
+            console.log('\n');
+
+        });
+    }
+}
+
+function doIt() {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        var output = data.split(",");
+        action = output[0];
+        value = output[1];
+
+        console.log('\n');
+        console.log(output);
+
+        switch (action) {
+            case "my-tweets":
+                twitter();
+                break;
+
+            case "spotify-this-song":
+                spotify();
+                break;
+
+            case "movie-this":
+                omdb();
+                break;
+
+            case "do-what-it-says":
+                doIt();
+                break;
+        }
+
+    });
+}
